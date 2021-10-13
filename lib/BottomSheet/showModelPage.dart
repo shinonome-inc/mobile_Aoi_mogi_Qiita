c7096ca958db4dc73769a7af73b828e0d8737f70
AoiNishinaka <crowned021303@gmail.com>
2021-10-13 11:32:55 +0900

    showBottomSheetの修正

--- lib/BottomSheet/showModelPage.dart
+++ lib/BottomSheet/showModelPage.dart
@@ -8,14 +8,6 @@ void showModalPage(BuildContext context, Widget page) {
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
     ),
-    builder: (context) => DraggableScrollableSheet(
-      initialChildSize: 1.0,
-      builder: (context, scrollController) => Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-        ),
-        child: page,
-      ),
-    ),
+    builder: (context) => page,
   );
 }
