1c1d44274f3f5761021b95a3802cea1671fd2ade
AoiNishinaka <crowned021303@gmail.com>
2021-12-11 03:04:02 +0900

    TagDetailPage用のListを追加

--- lib/Api/network_request.dart
+++ lib/Api/network_request.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:aoi_mogi_qiita/Api/model/tag_detail.dart';
 import 'package:http/http.dart' as http;
 
 import 'model/article.dart';
@@ -24,6 +25,18 @@ class Qiita {
     if (response.statusCode == 200) {
       final List<dynamic> jsonArray = json.decode(response.body);
       return jsonArray.map((json) => Tag.fromJson(json)).toList();
+    } else {
+      throw Exception('読み込めませんでした');
+    }
+  }
+
+  static Future<List<TagArticle>> fetchTagArticle(String tagId) async {
+    final response = await http.get(Uri.parse('https://qiita.com/api/v2/tags/' +
+        tagId +
+        '/items?page=1&per_page=20'));
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonArray = json.decode(response.body);
+      return jsonArray.map((json) => TagArticle.fromJson(json)).toList();
     } else {
       throw Exception('読み込めませんでした');
     }
