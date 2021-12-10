ebad1b69be41b44b08fec4c66dec5ad87059420e
AoiNishinaka <crowned021303@gmail.com>
2021-12-11 03:10:38 +0900

    HexColorの追加

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,5 @@ final Color bottomGray = const Color(0xFF828282);
 final Color buttonGreen = const Color(0xFF468300);
 final Color bottomGreen = const Color(0xFF74C13A);
 final Color articlegrey = const Color(0x828282);
+final Color backGreen = const Color(0xFF468300);
+final Color tagGray = const Color(0xFFF2F2F2);
